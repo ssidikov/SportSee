c4cae9165aab24ce4dc035f68a2826c8e72742ba
ssidikov <sardorbek.sidikov7@gmail.com>
2024-11-08 16:36:29 +0100

    Refactor Sidebar component: update class naming convention and improve styles for consistency

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -10,31 +10,31 @@ const currentYear = new Date().getFullYear()
 function Sidebar() {
   return (
     <aside className='sidebar'>
-      <nav className='sidebar-nav'>
-        <ul className='sidebar-nav-list'>
-          <li className='sidebar-nav-list-link'>
-            <NavLink to='/fitness'>
-              <img src={LogoFitness} alt='Fitness' />
+      <nav className='sidebar__nav'>
+        <ul className='sidebar__list'>
+          <li className='sidebar__item'>
+            <NavLink to='/fitness' className='sidebar__link'>
+              <img src={LogoFitness} alt='Fitness' className='sidebar__icon' />
             </NavLink>
           </li>
-          <li className='sidebar-nav-list-link'>
-            <NavLink to='/cycling'>
-              <img src={LogoCycling} alt='Cycling' />
+          <li className='sidebar__item'>
+            <NavLink to='/cycling' className='sidebar__link'>
+              <img src={LogoCycling} alt='Cycling' className='sidebar__icon' />
             </NavLink>
           </li>
-          <li className='sidebar-nav-list-link'>
-            <NavLink to='/swim'>
-              <img src={LogoSwim} alt='Swim' />
+          <li className='sidebar__item'>
+            <NavLink to='/swim' className='sidebar__link'>
+              <img src={LogoSwim} alt='Swim' className='sidebar__icon' />
             </NavLink>
           </li>
-          <li className='sidebar-nav-list-link'>
-            <NavLink to='/meditation'>
-              <img src={LogoMeditation} alt='Meditation' />
+          <li className='sidebar__item'>
+            <NavLink to='/meditation' className='sidebar__link'>
+              <img src={LogoMeditation} alt='Meditation' className='sidebar__icon' />
             </NavLink>
           </li>
         </ul>
       </nav>
-      <p className='sidebar-text'>Copyright, SportSee {currentYear}</p>
+      <p className='sidebar__text'>Copyright, SportSee {currentYear}</p>
     </aside>
   )
 }
