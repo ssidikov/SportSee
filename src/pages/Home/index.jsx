97ea411a45ac649db45cd106d44139c94bd36903
ssidikov <sardorbek.sidikov7@gmail.com>
2024-11-08 17:20:01 +0100

    Refactor Home component styles and structure: update class names, improve layout, and add user avatar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,17 @@
 import './Home.sass'
 import { Link } from 'react-router-dom'
+import UserAvatar from '../../assets/avatar.svg'
 
 function Home() {
   return (
     <div className='home'>
-      <h2 className='home-title'>Choisissez un utilisateur pour voir ses détails</h2>
-      <Link to='/profile' className='home__user-container'>
-        Cecilia
+      <h2 className='home__title'>Choisissez un utilisateur pour voir ses détails</h2>
+      <Link to='/profile' className='home__user-card'>
+        <img src={UserAvatar} alt='User Avatar' className='home__user-avatar' />
+        <div className='home__user-info'>
+          <span className='home__user-name'>Cecilia</span>
+          <span className='home__user-status'>Profil actif</span>
+        </div>
       </Link>
     </div>
   )
